e960a7d8333695cd7632e42a2a7c149b0ef50bab
AmeeQAQ <xivsagitemperance@gmail.com>
2024-07-08 15:39:54 +0200

    removed use of env vars

--- src/rrw.sh
+++ src/rrw.sh
@@ -1,5 +1,46 @@
 #!/bin/bash
 
+######################
+## GLOBAL VARIABLES ##
+######################
+
+# Game running
+detected=""
+
+# Is the game still running?
+proc_flag=0
+
+# RRW default config dir
+rrw_dir=$HOME/.config/rrw
+
+# RRW default games.json location
+rrw_json=$HOME/.config/rrw/games.json
+
+####################################
+# Gather system data from rrw.conf #
+####################################
+
+# Wayland or X11?
+session_type=$(grep -e "^session_type" $rrw_dir/rrw.conf | awk '{print $3}')
+
+wayland_flag=0
+
+if [[ "$session_type" == "wayland" ]]; then
+	wayland_flag=1
+fi
+
+# User DE
+current_desktop=$(grep -e "^current_desktop" $rrw_dir/rrw.conf | awk '{print $3}')
+
+# Screen properties
+screen_output_name=$(grep -e "^screen_output_name" $rrw_dir/rrw.conf | awk '{print $3}')
+screen_resolution=$(grep -e "^screen_resolution" $rrw_dir/rrw.conf | awk '{print $3}')
+screen_refresh_rate=$(grep -e "^screen_refresh_rate" $rrw_dir/rrw.conf | awk '{print $3}')
+
+######################
+## Script functions ##
+######################
+
 # Function to check for each game inside the game list
 game_check() {
 	for game in ${game_list[@]}
@@ -14,7 +55,7 @@ game_check() {
 
 # Function to change the screen's refresh rate
 rr_change() {
-	if [[ "$session_type" == "wayland" ]]; then
+	if [ $wayland_flag -eq 1 ]; then
 		if [[ "$current_desktop" == "KDE" ]]; then
 			kscreen-doctor output.$screen_output_name.mode.$screen_resolution@$1
 		elif echo "$current_desktop" | grep "GNOME" &>/dev/null; then
@@ -27,17 +68,15 @@ rr_change() {
 	fi
 }
 
+#############
+## Prelude ##
+#############
+
 # Make sure there's no parameters
 if [[ ! -z "$@"  ]]; then
 	echo "Unknown parameter(s), please run RRW with no parameters."
 	exit 1
 fi
-
-# RRW default config dir
-rrw_dir=$HOME/.config/rrw
-
-# RRW default games.json location
-rrw_json=$HOME/.config/rrw/games.json
 
 # Check games.json existence. If it exists, check that it is not empty
 if [ ! -f "$rrw_json" ]; then
@@ -48,45 +87,16 @@ elif [[ -z $(grep '[^[:space:]]' $rrw_json) ]]; then
 	exit 1
 fi
 
-detected=""
-
-proc_flag=0
-
-systemctl --user show-environment
-
-# Gather system data
-session_type=$(grep -e "^session_type" $rrw_dir/rrw.conf | awk '{print $3}')
-current_desktop=$(grep -e "^current_desktop" $rrw_dir/rrw.conf | awk '{print $3}')
-display=$(grep -e "^display" $rrw_dir/rrw.conf | awk '{print $3}')
-
-xauth=$(systemctl --user show-environment | grep XAUTHORITY | cut -d '=' -f 2)
-
-screen_output_name=$(grep -e "^screen_output_name" $rrw_dir/rrw.conf | awk '{print $3}')
-screen_resolution=$(grep -e "^screen_resolution" $rrw_dir/rrw.conf | awk '{print $3}')
-screen_refresh_rate=$(grep -e "^screen_refresh_rate" $rrw_dir/rrw.conf | awk '{print $3}')
-
-# Set environmental variables for RRW to work at startup
-export DISPLAY=$display
-export XAUTHORITY=$xauth
-export XDG_SESSION_TYPE=$session_type
-export XDG_CURRENT_DESKTOP=$current_desktop
-
 # Gather games from games.json
 mapfile game_list < <(jq '.games[].name' $rrw_json | tr -d '"')
 
-# Main loop
+###############
+## Main loop ##
+###############
+
 while true
 do
 	echo "[RRW] Looking for games"
-	
-	echo "var $display"
-	echo "env $DISPLAY"
-	echo "var $xauth"
-	echo "env $XAUTHORITY"
-	echo "var $session_type"
-	echo "env $XDG_SESSION_TYPE"
-	echo "var $current_desktop"
-	echo "env $XDG_CURRENT_DESKTOP"
 
 	while [ $proc_flag -eq 0 ]
 	do
